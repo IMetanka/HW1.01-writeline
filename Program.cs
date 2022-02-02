d4d99bd77b6e31453a4d9ee079ef6c0b05657553
1 <1@DESKTOP-LQ88GBK>
2022-02-02 22:22:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp1
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Самое начало кода
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
